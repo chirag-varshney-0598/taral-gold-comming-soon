525dca40a2922953f2838096a39af5736a297b96
Chirag <ckvarshney8@gmail.com>
2023-10-23 23:15:24 +0530

    added the site meta tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,6 +12,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="description"
+          content="A Next Generation Crypto Banking Platform"
+        />
+        <meta property="og:title" content="Taral Gold" />
+        <meta
+          property="og:description"
+          content="A Next Generation Crypto Banking Platform"
+        />
+        <meta property="og:image" content="/images/gold.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <link rel="icon" href="/images/gold.png" />
+        {/* <link rel="icon" href="/images/gold.png" /> */}
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
